36cbbfa11603b677199873d5f81ad13b5ce9d455
irfans-git <toirfanshamim@gmail.com>
2022-01-05 23:23:58 -0500

    terraform mod

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-0424d9be6e6eb4023"
+}
+variable "amiID" {
+	default="ami-0b0af3577fe5e3532"
+}
+variable "keyPair" {
+	default="./jenkins"
+}
+
+
+
+
